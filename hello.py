778d045b3795c71a6dcc75293565891b080764df
niikun <niikun0209@gmail.com>
2024-06-15 14:26:41 +0900

    render_template

--- hello.py
+++ hello.py
@@ -1,15 +1,18 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+name_list = [
+    "Alice",
+    "Bob",
+    "Cathy",
+    "David"
+]
 
-@app.route("/hello/<name>")
-def hello_name(name):
-    text = f"Hello, {name}"
-    return text
+@app.route("/<name>")
+def hello(name):
+    return render_template("index.html",name=name,name_lists=name_list)
 
 if __name__=="__main__":
     app.run(debug=True)
