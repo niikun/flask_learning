7c666172aea6341cf95fef2d0f33b2945e88a9d5
niikun <niikun0209@gmail.com>
2024-06-16 16:59:09 +0900

    memo_list

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from flask import render_template
 app = Flask(__name__)
 
 
-memo = [
+memo_list = [
     ["memo1","content1"],
     ["memo2","content2"],
     ["memo3","content3"]
 ]
 
 @app.route("/")
-def top(memo=memo):
-    return render_template("index.html",memo=memo)
+def top():
+    return render_template("index.html",memo_list=memo_list)
 
 if __name__=="__main__":
     app.run(debug=True)
