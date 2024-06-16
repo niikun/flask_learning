1e9aaee757348ffded7d2dd55993ebad1dd82736
niikun <niikun0209@gmail.com>
2024-06-16 16:48:31 +0900

    index.html update

--- app.py
+++ app.py
@@ -3,9 +3,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+memo = [
+    ["memo1","content1"],
+    ["memo2","content2"],
+    ["memo3","content3"]
+]
+
 @app.route("/")
-def top():
-    return render_template("index.html")
+def top(memo=memo):
+    return render_template("index.html",memo=memo)
 
 if __name__=="__main__":
     app.run(debug=True)
