e14eba02656d490fc6393c0e38a685afec3714f6
niikun <123465566+niikun@users.noreply.github.com>
2024-12-23 14:36:51 +0000

    block content

---
+++ app2.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+list = [
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+    "test6",
+    "test7",
+]
+
+@app.route("/<name>")
+def index(name):
+    return render_template("index.html", name=name, items=list)
+
+
+if __name__ == "__main__":
+    app.run()
